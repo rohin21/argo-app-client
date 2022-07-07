0f7ba2732174fd3f1e29e56343750386344b47f9
rohin <rohinpandey21@gmail.com>
2022-07-07 17:18:26 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { Fragment } from "react";
+import Otherpage from "./Otherpage";
+import MainComponent from "./MainComponent";
+import "./App.css";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Fragment>
+        <header className="header">
+          <div>This is a multicontainer application</div>
+          <Link to="/">Home</Link>
+          <Link to="/otherpage">Other Page</Link>
+        </header>
+        <div className="main">
+          <Route exact path="/" component={MainComponent} />
+          <Route exact path="/otherpage" component={Otherpage} />
+        </div>
+      </Fragment>
+    </Router>
   );
 }
 
---
+++ src/Otherpage.js
@@ -0,0 +1,14 @@
+import { Link } from "react-router-dom";
+
+import React from "react";
+
+function Otherpage() {
+  return (
+    <div>
+      Otherpage
+      <Link to="/">Go Back to Home</Link>
+    </div>
+  );
+}
+
+export default Otherpage;
